164fc11f4544fc39dcb6949b0e85ce87795af47a
Philippe Desgroseilliers <philippe.desgroseilliers@polymtl.ca>
2018-02-02 23:55:42 -0500

    iLv370 Diamon WHM Gear String Attribute finished

--- Currency Calculator FFXIV/WHMGearList.cs
+++ Currency Calculator FFXIV/WHMGearList.cs
@@ -1,99 +1,133 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Currency_Calculator_FFXIV
+﻿namespace Currency_Calculator_FFXIV
 {
-    public enum WHMGearList
+    public enum WhmGearList
     {
         //iLv350
+
         [StringValue("Carborundum Circlet of Healing")]
-        CARBORUNDUM_HEAD,
+        CARBORUNDUM_HEAD = 0,
 
         [StringValue("Carborundum Robe of Healing")]
-        CARBORUNDUM_BODY,
+        CARBORUNDUM_BODY = 1,
 
         [StringValue("Carborundum Gloves of Healing")]
-        CARBORUNDUM_HANDS,
+        CARBORUNDUM_HANDS = 2,
 
         [StringValue("Carborundum Tassets of Healing")]
-        CARBORUNDUM_WAIST,
+        CARBORUNDUM_WAIST = 3,
 
         [StringValue("Carborundum Trousers of Healing")]
-        CARBORUNDUM_LEGS,
+        CARBORUNDUM_LEGS = 4,
 
         [StringValue("Carborundum Boots of Healing")]
-        CARBORUNDUM_BOOTS,
+        CARBORUNDUM_BOOTS = 5,
 
         [StringValue("Carborundum Earring of Healing")]
-        CARBORUNDUM_EARRINGS,
+        CARBORUNDUM_EARRINGS = 6,
 
         [StringValue("Carborundum Necklace of Healing")]
-        CARBORUNDUM_NECKLACE,
+        CARBORUNDUM_NECKLACE = 7,
 
         [StringValue("Carborundum Bracelet of Healing")]
-        CARBORUNDUM_BRACELET,
+        CARBORUNDUM_BRACELET = 8,
 
         [StringValue("Carborundum Ring of Healing")]
-        CARBORUNDUM_RING,
+        CARBORUNDUM_RING = 9,
 
         //iLv360
 
         [StringValue("Ryumyaku Hitai-Ate of Healing")]
-        RYUMYAKU_HEAD,
+        RYUMYAKU_HEAD = 10,
+
         [StringValue("Ryumyaku Dogi of Healing")]
-        RYUMYKAU_BODY,
+        RYUMYKAU_BODY = 11,
+
         [StringValue("Ryumyaku Kote of Healing")]
-        RYUMYAKU_HANDS,
+        RYUMYAKU_HANDS = 12,
+
         [StringValue("Ryumyaku Obi of Healing")]
-        RYUMYAKU_WAIST,
+        RYUMYAKU_WAIST = 13,
+
         [StringValue("Ryumyaku Tsutsu-Hakama of Healing")]
-        RYUMYAKU_LEGS,
+        RYUMYAKU_LEGS = 14,
+
         [StringValue("Ryumyaku Choka of Healing")]
-        RYUMYAKU_BOOTS,
+        RYUMYAKU_BOOTS = 15,
+
         [StringValue("Ryumyaku Earring of Healing")]
-        RYUMYAKU_EARRINGS,
+        RYUMYAKU_EARRINGS = 16,
+
         [StringValue("Ryumyaku Necklace of Healing")]
-        RYUMYAKU_NECKLACE,
+        RYUMYAKU_NECKLACE = 17,
+
         [StringValue("Ryumyaku Bracelet of Healing")]
-        RYUMYAKU_BRACELET,
+        RYUMYAKU_BRACELET = 18,
+
         [StringValue("Ryumyaku Ring of Healing")]
-        RYUMYAKU_RING,
+        RYUMYAKU_RING = 19,
 
         //iLv370 Dai-Ryumyaku
+
         [StringValue("Dai-Ryumyaku Hitai-Ate of Healing")]
-        DAI_RYUMYAKU_HEAD,
+        DAI_RYUMYAKU_HEAD = 20,
+
         [StringValue("Dai-Ryumyaku Dogi of Healing ")]
-        DAI_RYUMYAKU_BODY,
+        DAI_RYUMYAKU_BODY = 21,
+
         [StringValue("Dai-Ryumyaku Kote of Healing")]
-        DAI_RYUMYAKU_HANDS,
+        DAI_RYUMYAKU_HANDS = 22,
+
         [StringValue("Dai-Ryumyaku Obi of Healing")]
-        DAI_RYUMYAKU_WAIST,
+        DAI_RYUMYAKU_WAIST = 23,
+
         [StringValue("Dai-Ryumyaku Tsutsu-Hakama of Healing")]
-        DAI_RYUMYAKU_LEGS,
+        DAI_RYUMYAKU_LEGS = 24,
+
         [StringValue("Dai-Ryumyaku Choka of Healing")]
-        DAI_RYUMYAKU_BOOTS,
+        DAI_RYUMYAKU_BOOTS = 25,
+
         [StringValue("Dai-Ryumyaku Earring of Healing")]
-        DAI_RYUMYAKU_EARRINGS,
+        DAI_RYUMYAKU_EARRINGS = 26,
+
         [StringValue("Dai-Ryumyaku Necklace of Healing")]
-        DAI_RYUMYAKU_NECKLACE,
+        DAI_RYUMYAKU_NECKLACE = 27,
+
         [StringValue("Dai-Ryumyaku Bracelet of Healing")]
-        DAI_RYUMYAKU_BRACELET,
+        DAI_RYUMYAKU_BRACELET = 28,
+
         [StringValue("Dai-Ryumyaku Bracelet of Healing")]
-        DAI_RYUMYAKU_RING,
+        DAI_RYUMYAKU_RING = 29,
 
         //iLv370 Diamond
 
-        DIAMOND_HEAD,
-        DIAMOND_BODY,
-        DIAMOND_HANDS,
-        DIAMOND_LEGS,
-        DIAMOND_BOOTS,
-        DIAMOND_EARRINGS,
-        DIAMOND_NECKLACE,
-        DIAMOND_BRACELET,
-        DIAMOND_RUNG
+        [StringValue("Diamond Circlet of Healing")]
+        DIAMOND_HEAD = 30,
+
+        [StringValue("Diamond Robe of Healing")]
+        DIAMOND_BODY = 31,
+
+        [StringValue("Diamond Gloves of Healing")]
+        DIAMOND_HANDS = 32,
+
+        [StringValue("Diamond Tassets of Healing")]
+        DIAMOND_WAIST = 33,
+
+        [StringValue("Diamond Trousers of Healing")]
+        DIAMOND_LEGS = 34,
+
+        [StringValue("Diamond Boots of Healing")]
+        DIAMOND_BOOTS = 35,
+
+        [StringValue("Diamond Earring of Healing")]
+        DIAMOND_EARRINGS = 36,
+
+        [StringValue("Diamond Necklace of Healing")]
+        DIAMOND_NECKLACE = 37,
+
+        [StringValue("Diamond Bracelet of Healing")]
+        DIAMOND_BRACELET = 38,
+
+        [StringValue("Diamond Ring of Healing")]
+        DIAMOND_RING = 39
     }
 }
