3967916434a904c5bf87cc30a4a6cacb97f48243
Philippe Desgroseilliers <philippe.desgroseilliers@polymtl.ca>
2018-02-07 16:04:02 -0500

    Added Currency and Price attributes for Enums, changed name of Form1 to MainForm

---
+++ Currency Calculator FFXIV/Gear List/CurrencyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Currency_Calculator_FFXIV.Gear_List
+{
+    public class CurrencyAttribute : Attribute
+    {
+        public string Currency { get; }
+
+        public CurrencyAttribute(string currency)
+        {
+            Currency = currency;
+        }
+    }
+}
---
+++ Currency Calculator FFXIV/Gear List/PriceAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Currency_Calculator_FFXIV.Gear_List
+{
+    public class PriceAttribute : Attribute
+    {
+        public string Price { get; }
+
+        public PriceAttribute(string price)
+        {
+            Price = price;
+        }
+    }
+}
