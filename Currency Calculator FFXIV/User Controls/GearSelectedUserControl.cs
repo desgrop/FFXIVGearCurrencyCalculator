46cec830c7f30ec0267cfe6ab665f003408d7537
Philippe Desgroseilliers <philippe.desgroseilliers@polymtl.ca>
2018-02-07 16:43:10 -0500

    Created GearSelectedUserControl file. No work done

---
+++ Currency Calculator FFXIV/User Controls/GearSelectedUserControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency_Calculator_FFXIV.User_Controls
+{
+    public partial class GearSelectedUserControl : UserControl
+    {
+        public GearSelectedUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
